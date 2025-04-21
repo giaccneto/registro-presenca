dbdbbe10ca3e75635a6895918717e19da965862d
giaccneto <giaccneto@hotmail.com>
2025-04-21 18:47:27 -0400

    Inicializando projeto

---
+++ src/main/java/com/giaccento/registro_presenca/RegistroPresencaApplication.java
@@ -0,0 +1,13 @@
+package com.giaccento.registro_presenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroPresencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroPresencaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giaccento/registro_presenca/RegistroPresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giaccento.registro_presenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroPresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
