4c826e1b6debc0869d635456085b55fd279044bd
Giacomo Accardi Neto <44924062+giaccneto@users.noreply.github.com>
2025-04-23 15:59:15 -0400

    Update UsuarioService.java

--- src/main/java/com/giaccento/registro_presenca/service/UsuarioService.java
+++ src/main/java/com/giaccento/registro_presenca/service/UsuarioService.java
@@ -3,6 +3,7 @@ package com.giaccento.registro_presenca.service;
 
 import com.giaccento.registro_presenca.entity.Usuario;
 import com.giaccento.registro_presenca.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,14 +13,21 @@ import java.util.List;
 @Service
 public class UsuarioService {
 
-    public final UsuarioRepository usuarioRepository;
+    @Autowired
+    public UsuarioRepository usuarioRepository;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
-    }
 
     public Usuario resgistrarPresenca(Usuario usuario){
-        return usuarioRepository.save(usuario);
+        if(isCpfValido(usuario)) {
+             return usuarioRepository.save(usuario);
+        }else {
+            throw new IllegalArgumentException("Erro, CPF no formato inválido!");
+        }
+    }
+
+    public boolean isCpfValido(Usuario usuario){
+        String cpf = usuario.getCpf();
+        return cpf != null  && cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}");
     }
 
     public List<Usuario> buscarPresencas(){
