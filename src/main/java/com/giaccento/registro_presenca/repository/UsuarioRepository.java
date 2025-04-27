d842d09a9c60dfbd5f2fb48b02623ed0df658f42
giaccneto <giaccneto@hotmail.com>
2025-04-27 14:18:17 -0400

    Adicionados no repository busca por email e deletar por email

--- src/main/java/com/giaccento/registro_presenca/repository/UsuarioRepository.java
+++ src/main/java/com/giaccento/registro_presenca/repository/UsuarioRepository.java
@@ -3,7 +3,17 @@ package com.giaccento.registro_presenca.repository;
 import com.giaccento.registro_presenca.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
